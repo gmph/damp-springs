db3c8568379869016c781c1417a24a8dda0b86e6
Graham Macphee <grahammacphee@MacBook-Pro-7.local>
2016-12-17 23:15:06 +0000

    Reverted

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response){
-	console.dir(request.body);
-	reponse.writeHead(200, {'Content-Type' : 'application/json'});
-	response.json({
-		'online' : true
-	});
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.end('bye');
 });
 
-app.post('/test', function(request, response){
-	console.dir(request.body);
-	response.writeHead(200, {'Content-Type': 'application/json'});
-	response.json({
-		'test' : true
-	});
+app.post('/test', function(req, res){
+	console.dir(req.body);
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.end('thanks');
 });

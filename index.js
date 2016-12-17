e9b027d6caa8bc2385fcdb178ab1e08e1d39370d
Graham Macphee <grahammacphee@MacBook-Pro-7.local>
2016-12-17 22:59:24 +0000

    Updated meta, removed extra dependencies

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
 app.get('/', function(request, response){
 	console.dir(request.body);
 	reponse.writeHead(200, {'Content-Type' : 'application/json'});
